2aa6cccd7857b44ed3e4b041259f4d9635081c5e
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2021-02-27 10:28:51 +0330

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,1 +1,15 @@
+from rivescript import RiveScript
 
+bot = RiveScript(utf8=True)
+bot.load_directory("./Body")
+bot.sort_replies()
+
+
+def reply(text: str) -> str:
+    res = bot.reply("localuser", text)
+    return res if res != "[ERR: No Reply Matched]" else ""
+
+
+if __name__ == "__main__":
+    while True:
+        print(reply(input()))
